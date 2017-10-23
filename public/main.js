3ed62c4711f1c14d306206bb6dc437192bac7108
Ronna Mae Firmo <ronnwiseley@gmail.com>
2017-10-23 11:41:22 +0800

    feat: Modify navbar & introduce spa

---
+++ public/main.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+  const bodyContent = document.getElementById('bodyContent');
+
+  // links
+  const homeViewLink = document.getElementById('homeViewLink');
+  const customerRegistrationViewLink = document.getElementById('customerRegistrationViewLink');
+  const packageRegistrationViewLink = document.getElementById('packageRegistrationViewLink');
+  const allCustomersViewLink = document.getElementById('allCustomersViewLink');
+  const customerSearchViewLink = document.getElementById('customerSearchViewLink');
+
+  // templates
+  const customerRegistrationView = document.getElementById('customerRegistrationView');
+
+  function setContentView(view) {
+      bodyContent.innerHTML = view.innerHTML;
+  }
+
+  function prepairLinkForSwitchingView(link, view) {
+    link.onclick = (event) => {
+      event.preventDefault();
+      setContentView(view);
+    }
+  }
+
+  setContentView(customerRegistrationView);
+  prepairLinkForSwitchingView(customerRegistrationViewLink, customerRegistrationView);
+}
