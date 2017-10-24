0ca4fca863f18013bfe63413fdafe06e2d092de2
Ronna Mae Firmo <ronnwiseley@gmail.com>
2017-10-25 00:47:45 +0800

    fix: Fix messy navbar into nav pills

--- public/main.js
+++ public/main.js
@@ -3,20 +3,14 @@ window.onload = () => {
 
   // links
   const homeViewLink = document.getElementById('homeViewLink');
-  const customerRegistrationViewLink = document.getElementById('customerRegistrationViewLink');
-  const packageRegistrationViewLink = document.getElementById('packageRegistrationViewLink');
-  const allCustomersViewLink = document.getElementById('allCustomersViewLink');
-  const customerSearchViewLink = document.getElementById('customerSearchViewLink');
-  const packageTrackViewLink = document.getElementById('packageTrackViewLink');
+  const customersViewLink = document.getElementById('customersViewLink');
+  const packagesViewLink = document.getElementById('packagesViewLink');
 
   // templates
   const adminLoginView = document.getElementById('adminLoginView');
   const homeView = document.getElementById('homeView');
-  const customerRegistrationView = document.getElementById('customerRegistrationView');
-  const packageRegistrationView = document.getElementById('packageRegistrationView');
-  const allCustomersView = document.getElementById('allCustomersView');
-  const customerSearchView = document.getElementById('customerSearchView');
-  const packageTrackView = document.getElementById('packageTrackView');
+  const customersView = document.getElementById('customersView');
+  const packagesView = document.getElementById('packagesView');
 
   function setContentView(view) {
       bodyContent.innerHTML = view.innerHTML;
@@ -31,19 +25,85 @@ window.onload = () => {
     }
   }
  
-  setContentView(packageRegistrationView);
-  
-  prepairLinkForSwitchingView(customerRegistrationViewLink, customerRegistrationView, () => {
-    console.log('switched to customer registration view')
-  });
+  setContentView(homeView);
 
   prepairLinkForSwitchingView(homeViewLink, homeView, () => {
     console.log('switched to home view');
   });
-  
-  prepairLinkForSwitchingView(packageTrackViewLink, packageTrackView, () => {
-    console.log('switched to package track view');
 
+  prepairLinkForSwitchingView(customersViewLink, customersView, () => {
+    console.log('switched to customers view');
+
+    // getting all registered customers
+    doAjax(`/getCustomers`, (customers) => {
+      const customersContainer = document.getElementById('customersContainer');
+      customersContainer.innerHTML = '';
+
+      for (const customer of customers) {
+        customersContainer.innerHTML += `
+          <div><h5><i class="fa fa-user-o" aria-hidden="true"></i> <strong>${customer.name}</strong></h5></div>
+          <div class="row">
+            <div class="col-lg-3"><i class="fa fa-envelope-o" aria-hidden="true"></i> ${customer.email}</div>
+            <div class="col-md-2"><i class="fa fa-mobile" aria-hidden="true"></i> ${customer.mobileNumber}</div>
+            <div class="col-md-auto"><i class="fa fa-address-book-o" aria-hidden="true"></i> ${customer.address}</div>
+          </div>
+          <hr>
+        `;
+      }
+    })
+
+    // finding a certain customer
+    findCustomerInput.onkeyup = () => {
+      const findCustomerInput = document.getElementById('findCustomerInput');
+
+      doAjax(`/findCustomer/${encodeURIComponent(findCustomerInput.value)}`, (customers) => {
+        const findCustomersContainer = document.getElementById('findCustomersContainer'); 
+        findCustomersContainer.innerHTML = '';
+
+        for (const customer of customers) {
+          findCustomersContainer.innerHTML += `
+            <div><h5><i class="fa fa-user-o" aria-hidden="true"></i> <strong>${customer.name}</strong></h5></div>
+            <div class="row">
+              <div class="col-lg-3"><i class="fa fa-envelope-o" aria-hidden="true"></i> ${customer.email}</div>
+              <div class="col-md-2"><i class="fa fa-mobile" aria-hidden="true"></i> ${customer.mobileNumber}</div>
+              <div class="col-md-auto"><i class="fa fa-address-book-o" aria-hidden="true"></i> ${customer.address}</div>
+            </div>
+            <hr>
+          `;
+        }
+      })
+    }
+  });
+
+  prepairLinkForSwitchingView(packagesViewLink, packagesView, () => {
+    // for package registration from
+    const originInput = document.getElementById('originInput');
+    const destinationInput = document.getElementById('destinationInput');
+    
+    doAjax('/getCoveredAreas', (areas) => {
+      originInput.innerHTML = '';
+      destinationInput.innerHTML = '';
+
+      for (const area of areas) {
+        const optionTag = `<option value="${area.address}">${area.address}</option>`;
+
+        originInput.innerHTML += optionTag; 
+        destinationInput.innerHTML += optionTag; 
+      }
+    })
+
+    doAjax(`/getPackages/59e649e8110f0b163a701e8d`, (packages) => {
+      const addedPackagesContainer = document.getElementById('addedPackagesContainer');
+      addedPackagesContainer.innerHTML = '';
+
+      for (const package of packages) {
+        addedPackagesContainer.innerHTML += `
+          <div><i class="fa fa-archive" aria-hidden="true"></i> package id: ${package._id}</div>
+        `
+      } 
+    });
+
+    // for finding packages
     const findPackageInput = document.getElementById('findPackageInput');
 
     findPackageInput.onkeyup = () => {
@@ -94,80 +154,5 @@ window.onload = () => {
         `;
       })
     }
-  });
-
-  prepairLinkForSwitchingView(allCustomersViewLink, allCustomersView, () => {
-    console.log('switched to all customers view');
-    
-    doAjax(`/getCustomers`, (customers) => {
-      const customersContainer = document.getElementById('customersContainer');
-      customersContainer.innerHTML = '';
-
-      for (const customer of customers) {
-        customersContainer.innerHTML += `
-          <div><h5><i class="fa fa-user-o" aria-hidden="true"></i> <strong>${customer.name}</strong></h5></div>
-          <div class="row">
-            <div class="col-lg-3"><i class="fa fa-envelope-o" aria-hidden="true"></i> ${customer.email}</div>
-            <div class="col-md-2"><i class="fa fa-mobile" aria-hidden="true"></i> ${customer.mobileNumber}</div>
-            <div class="col-md-auto"><i class="fa fa-address-book-o" aria-hidden="true"></i> ${customer.address}</div>
-          </div>
-          <hr>
-        `;
-      }
-    })
-  });
-  
-  prepairLinkForSwitchingView(customerSearchViewLink, customerSearchView, () => {
-    console.log('switched to customer search view view');
-    
-    findCustomerInput.onkeyup = () => {
-      const findCustomerInput = document.getElementById('findCustomerInput');
-
-      doAjax(`/findCustomer/${encodeURIComponent(findCustomerInput.value)}`, (customers) => {
-        const findCustomersContainer = document.getElementById('findCustomersContainer'); 
-        findCustomersContainer.innerHTML = '';
-
-        for (const customer of customers) {
-          findCustomersContainer.innerHTML += `
-            <div><h5><i class="fa fa-user-o" aria-hidden="true"></i> <strong>${customer.name}</strong></h5></div>
-            <div class="row">
-              <div class="col-lg-3"><i class="fa fa-envelope-o" aria-hidden="true"></i> ${customer.email}</div>
-              <div class="col-md-2"><i class="fa fa-mobile" aria-hidden="true"></i> ${customer.mobileNumber}</div>
-              <div class="col-md-auto"><i class="fa fa-address-book-o" aria-hidden="true"></i> ${customer.address}</div>
-            </div>
-            <hr>
-          `;
-        }
-      })
-    }
-  });
-
-  // for package registration view
-  if (bodyContent.innerHTML == packageRegistrationView.innerHTML) {
-    const originInput = document.getElementById('originInput');
-    const destinationInput = document.getElementById('destinationInput');
-    
-    doAjax('/getCoveredAreas', (areas) => {
-      originInput.innerHTML = '';
-      destinationInput.innerHTML = '';
-
-      for (const area of areas) {
-        const optionTag = `<option value="${area.address}">${area.address}</option>`;
-
-        originInput.innerHTML += optionTag; 
-        destinationInput.innerHTML += optionTag; 
-      }
-    })
-
-    doAjax(`/getPackages/59e649e8110f0b163a701e8d`, (packages) => {
-      const addedPackagesContainer = document.getElementById('addedPackagesContainer');
-      addedPackagesContainer.innerHTML = '';
-
-      for (const package of packages) {
-        addedPackagesContainer.innerHTML += `
-          <div><i class="fa fa-archive" aria-hidden="true"></i> package id: ${package._id}</div>
-        `
-      } 
-    });
-  }
+  })
 }
